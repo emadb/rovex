2f67d34eaf6094e0ecd3d47e640aebc722364fc8
Emanuele DelBono <emanuele@codiceplastico.com>
2018-04-26 16:36:43 +0200

    better collision detection implementation

--- lib/world_map.ex
+++ lib/world_map.ex
@@ -13,32 +13,20 @@ defmodule WorldMap do
     GenServer.call(__MODULE__, {:update_rover, name, x, y})
   end
 
-
   def handle_call({:update_rover, name, x, y}, _from, state) do
     new_rovers = case Enum.find_index(state.rovers, fn r -> r.name == name end) do
       nil -> state.rovers ++ [%{name: name, x: x, y: y}]
       index -> List.replace_at(state.rovers, index, %{name: name, x: x, y: y})
     end
 
-    ## cerca duplicati
-    collision = Enum.filter(new_rovers, fn r -> r.name != name && r.x == x && r.y == y end)
-
-    if Enum.count(collision) > 0 do
-      ## uccidi duplicati
-      new_rovers
+    case Enum.any?(new_rovers, fn r -> r.name != name && r.x == x && r.y == y end) do
+      true -> new_rovers
         |> Enum.filter(fn r -> r.x == x && r.y == y end)
-        |> Enum.each(fn r ->
-            #pid = RegistryHelper.get_pid(r.name)
-            #Process.exit(pid, :kill)
-            state.rover_factory.kill(r.name)
-          end)
+        |> Enum.each(fn r -> state.rover_factory.kill(r.name) end)
+        new_rovers = Enum.reject(new_rovers, fn r -> r.x == x && r.y == y end)
+        {:reply, :ok, %{state | rovers: new_rovers}}
 
-      ## rimuovi duplicati dalla lista
-      new_rovers = Enum.reject(new_rovers, fn r -> r.x == x && r.y == y end)
+      false -> {:reply, :ok, %{state | rovers: new_rovers}}
     end
-
-    {:reply, :ok, %{state | rovers: new_rovers}}
   end
-
-
 end
