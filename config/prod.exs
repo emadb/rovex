850bc224688f3cdbf605eecf0612db24b0adea91
Emanuele DelBono <emanuele@codiceplastico.com>
2018-05-03 17:44:28 +0200

    parse string

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :rover,
-  http_port: {:system, "PORT"},
+  http_port: String.to_integer(System.get_env("PORT")),
   rover_supervisor: RoverSupervisor,
   world_width: 1000,
   world_height: 1000
