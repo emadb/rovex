3b2265bf70fba674fed6436f2feb664a66db5fd3
Emanuele DelBono <emanuele@codiceplastico.com>
2018-05-03 17:47:05 +0200

    cleanup config

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,4 @@
 use Mix.Config
 
 config :rover,
-  http_port: String.to_integer(System.get_env("PORT")),
-  rover_supervisor: RoverSupervisor,
-  world_width: 1000,
-  world_height: 1000
+  http_port: String.to_integer(System.get_env("PORT"))
