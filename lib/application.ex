055f29486d92f19d040537c6d560a585b0fe50cd
Emanuele DelBono <emanuele@codiceplastico.com>
2018-05-12 18:02:34 +0200

    format

--- lib/application.ex
+++ lib/application.ex
@@ -1,6 +1,5 @@
 defmodule Rover.Application do
   use Application
-  # use Supervisor
 
   @rover_supervisor Application.get_env(:rover, :rover_supervisor)
 
@@ -9,14 +8,7 @@ defmodule Rover.Application do
     children = [
       Supervisor.child_spec({Registry, [keys: :unique, name: Rover.Registry]}, id: :rover_registry),
       Supervisor.child_spec({Registry, [keys: :duplicate, name: Socket.Registry]}, id: :socket_registry),
-
-      Plug.Adapters.Cowboy.child_spec(
-        :http,
-        Rover.Web.Router,
-        [],
-        port: Settings.get_port(),
-        dispatch: dispatch()
-      ),
+      Plug.Adapters.Cowboy.child_spec(:http, Rover.Web.Router, [], port: Settings.get_port(), dispatch: dispatch()),
       Supervisor.child_spec({@rover_supervisor, []}, id: @rover_supervisor),
       Supervisor.child_spec({WorldMap, @rover_supervisor}, []),
     ]
