a8317581c84ab7aef0bd116b618e4bf4dc2f32dc
Emanuele DelBono <emanuele@codiceplastico.com>
2018-05-16 11:26:08 +0200

    more test

--- test/rover_integration_test.exs
+++ test/rover_integration_test.exs
@@ -1,6 +1,5 @@
 defmodule RoverIntegrationTest do
   use ExUnit.Case
-  doctest Rover
 
   test "send a series of commands (draw a square)" do
     {:ok, _} = Rover.start_link({1, 2, :N, "rover2"})
@@ -23,5 +22,28 @@ defmodule RoverIntegrationTest do
 
     {:ok, state} = Rover.get_state("rover2")
     assert state == {1, 2, :N, 0}
+  end
+
+  test "send a series of commands (draw a square backward)" do
+    {:ok, _} = Rover.start_link({5, 5, :N, "rover3"})
+
+    Rover.go_backward("rover3")
+    Rover.go_backward("rover3")
+    Rover.rotate_left("rover3")
+
+    Rover.go_backward("rover3")
+    Rover.go_backward("rover3")
+    Rover.rotate_left("rover3")
+
+    Rover.go_backward("rover3")
+    Rover.go_backward("rover3")
+    Rover.rotate_left("rover3")
+
+    Rover.go_backward("rover3")
+    Rover.go_backward("rover3")
+    Rover.rotate_left("rover3")
+
+    {:ok, state} = Rover.get_state("rover3")
+    assert state == {5, 5, :N, 0}
   end
 end
