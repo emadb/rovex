fe372446c7e96eb75a6fceb340ad89a4c585b5f8
Emanuele DelBono <emanuele@codiceplastico.com>
2024-05-21 15:33:31 +0200

    using :global

--- lib/rover.ex
+++ lib/rover.ex
@@ -2,15 +2,15 @@ defmodule Mars.Rover do
   use GenServer
 
   def start_link([id, {x, y}]) do
-    GenServer.start_link(__MODULE__, [id, {x, y}])
+    GenServer.start_link(__MODULE__, [id, {x, y}], name: {:via, :global, id})
   end
 
   def init([id, {x, y}]) do
     {:ok, %{id: id, pos: {x, y}, move_count: 0}}
   end
 
-  def get(pid) do
-    GenServer.call(pid, :get_state)
+  def get(id) do
+    GenServer.call({:via, :global, id}, :get_state)
   end
 
   def handle_call(:get_state, _from, state) do
