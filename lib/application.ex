f35d1a90c68d7abf3bc94299d1f31b60015e38b2
Emanuele DelBono <emanuele@codiceplastico.com>
2024-06-11 11:57:23 +0200

    remove comments

--- lib/application.ex
+++ lib/application.ex
@@ -1,8 +1,4 @@
 defmodule Mars.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
@@ -11,8 +7,6 @@ defmodule Mars.Application do
       {Mars.RoverSup, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Mars.Supervisor]
     Supervisor.start_link(children, opts)
   end
